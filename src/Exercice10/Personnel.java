af2a19eaa309bbc9a3fc5f15ea11d1295c9f6a3f
faridmo <jurispenale@gmail.com>
2018-01-09 20:18:30 +0000

    Ajout de l'exo 7 & 10

---
+++ src/Exercice10/Personnel.java
@@ -0,0 +1,76 @@
+package Exercice10;
+
+import java.util.Scanner;
+
+public class Personnel {
+
+	static private String nom,prenom,fonction,matricule;
+	static private char sexe;
+	static private int age;
+	
+	
+	public Personnel(String nom, String prenom, String matricule,String fonction, char sexe, int age) {
+		this.nom = nom;
+		this.prenom = prenom;
+		this.matricule=matricule;
+		this.fonction = fonction;
+		this.sexe = sexe;
+		this.age = age;
+	}
+	
+	
+	public static void main(String[] a){
+		
+		//Personnel p = new Personnel("macoumba","Thiam","Directeur",'M',12);
+		//Personnel p1 = new Personnel("fall","Binta","Directrice",'F',14);
+		
+		System.out.println("-----BIENVENU-----");
+		
+		Scanner clavier = new Scanner(System.in);
+		System.out.println("Saisissez votre sexe : ");
+		sexe=clavier.next().charAt(0);
+		if(sexe == 'M' || sexe == 'm'){
+			System.out.println("Entrez votre nom : ");
+			nom=clavier.next();
+			System.out.println("Entrez votre prenom : ");
+			prenom=clavier.next();
+			System.out.println("Entrez votre matricule : ");
+			matricule=clavier.next();
+			System.out.println("Entrez votre fonction au sein de l'entreprise : ");
+			fonction=clavier.next();
+			System.out.println("Entrez votre âge : ");
+			age=clavier.nextInt();
+		}else if(sexe == 'F' || sexe == 'f'){
+			System.out.println("Entrez votre nom : ");
+			nom=clavier.next();
+			System.out.println("Entrez votre prenom : ");
+			prenom=clavier.next();
+			System.out.println("Entrez votre matricule : ");
+			matricule=clavier.next();
+			System.out.println("Entrez votre fonction au sein de l'entreprise : ");
+			fonction=clavier.next();
+			System.out.println("Entrez votre âge : ");
+			age=clavier.nextInt();
+		}else{
+			System.out.println("Saisissez 'M' Pour Masculin et 'F' pour Féminin !");
+		}
+		
+		
+		System.out.println("---------------------------------");
+		System.out.println("Nom : "+nom);
+		System.out.println("Prenom : "+prenom);
+		System.out.println("Matricule : "+matricule);
+		System.out.println("Fonction : "+fonction);
+		System.out.print("Sexe : ");
+			if(sexe == 'M'){
+				System.out.println("Masculin");
+			}else if(sexe == 'F'){
+				System.out.println("Féminin");
+			}else{
+				System.out.println("Erreur");
+				clavier.close();
+			}
+		System.out.println("Age: "+age);
+		System.out.println("---------------------------------");
+	}
+}
---
+++ src/Exercice7/Cercle.java
@@ -0,0 +1,47 @@
+package Exercice7;
+
+import java.text.DecimalFormat;
+
+import Exercice6.Point;
+
+import static java.lang.Math.*;
+
+public class Cercle {
+
+	
+	Point centre;
+	double rayon,aire;
+	
+	DecimalFormat df = new DecimalFormat("#.##");
+	
+	public Cercle(Point centre, double rayon) {
+		this.centre = centre;
+		this.rayon = rayon;
+	}
+	
+	public double surface(double r){
+		aire = (3.14)*pow(r,2);
+		return aire;
+	}
+	
+	public void affiche(){
+		System.out.println("Le Cercle a un rayon de "+rayon+"cm de centre O");
+		centre.affiche();
+	}
+	
+	public void imprime(){
+		System.out.println("La surface du cercle de rayon "+rayon+" cm est : "+df.format(surface(rayon)));
+	}
+	
+	
+	public static void main(String[] args) {
+		Point p = new Point(2,3);
+		
+		Cercle c1 = new Cercle(p,4);
+		c1.affiche();
+		System.out.println("----------");
+		c1.imprime();
+	}
+
+	
+}
