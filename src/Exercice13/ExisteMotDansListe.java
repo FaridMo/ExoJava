1016dd79615548b0e2dbd6f6e9bcdd490cb7ccc2
faridmo <jurispenale@gmail.com>
2018-02-05 03:54:30 +0000

    exo 13 modf

--- src/Exercice13/ExisteMotDansListe.java
+++ src/Exercice13/ExisteMotDansListe.java
@@ -4,29 +4,26 @@ import java.util.Scanner;
 
 public class ExisteMotDansListe {
 
+	static Scanner clavier = new Scanner("System.in");
 	public static boolean existeDans(String mot,String[] liste){
-		boolean b=false;
 		mot=mot.toLowerCase();
 		for(int i=0;i<liste.length;i++){
-			if(liste[i].equals(mot)){
-				b=true;
+			if(liste[i] == mot){
+				return true;	
 			}
 			
 		}
-		return b;
+		return false;
 	}
 	
-	public static void affiche(){
-		String[] liste = {"bonjour","kasse","farid"};
-		Scanner clavier = new Scanner("System.in");
-		System.out.println("Entrez votre mot :");
-		String chaine = clavier.nextLine();
-		System.out.println(existeDans(chaine, liste));
-	}
 	
 	
 	public static void main(String[] args) {
-		affiche();
+		String[] liste = {"bonjour","kasse","farid","javase","javaee"};
+		
+		System.out.println(existeDans("javase", liste)); //true
+		System.out.println(existeDans("java", liste));  //false
+		
 		
 	}
 }
