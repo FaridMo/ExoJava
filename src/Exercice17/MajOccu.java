18f348675baa53a9cddb43676c22225ad0b336a9
faridmo <jurispenale@gmail.com>
2018-02-06 23:43:50 +0000

    AJOUT DE L'EXO 17

---
+++ src/Exercice17/MajOccu.java
@@ -0,0 +1,25 @@
+package Exercice17;
+
+import java.io.Console;
+import java.util.regex.*;
+
+public class MajOccu {
+	static Pattern pattern;
+	static Matcher matcher;
+	@SuppressWarnings({ "static-access"})
+	public static void Occu(String s,char c){
+		
+		pattern = pattern.compile("[A-Z]"); /*cette ligne permet de spécifier l'expression cherchée
+											*dans une chaine de caractère. Ici dans notre cas on 
+											*cherche les caractères de l'alphabet en Majuscule
+											*/
+		matcher = pattern.matcher(s);
+	
+	while(matcher.find()){	
+		
+		System.out.println(s.substring(matcher.groupCount(),matcher.end()));	
+		}
+	}
+	
+}
+
---
+++ src/Exercice17/TestMajOccu.java
@@ -0,0 +1,11 @@
+package Exercice17;
+
+public class TestMajOccu {
+
+	public static void main(String[] args) {
+											//| |	
+		MajOccu.Occu("anticonstitutionnellementT et constitutionnellement", 'T');
+
+	}
+
+}
